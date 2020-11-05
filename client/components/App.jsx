1b945ba2c6de3692088eed4e7a5ac34d67d6e0a9
olivia-gambitsis <o.gambitsis@gmail.com>
2020-11-06 08:05:40 +1300

    moving cookie

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,73 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component { 
+
+  state ={
+    style: {
+      backgroundColor: 'brown', 
+      borderStyle: 'solid',
+      borderColor: 'black',
+      height: '100px',
+      width: '50px',
+      gridArea: 'cookieLeft',
+      justifySelf: 'end'
+    },
+    style2:{
+      backgroundColor: 'brown', 
+      borderStyle: 'solid',
+      borderColor: 'black',
+      height: '100px',
+      width: '50px',
+      gridArea: 'cookieRight',
+      justifySelf: 'start'
+  
+    }
+  }
+
+  clickHandler = () => {
+    console.log('click')
+    this.setState({
+       style: {
+         ...this.state.style,
+         gridArea:'main',
+         justifySelf: 'start'
+       },
+       style2: {
+        ...this.state.style2,
+        gridArea: 'sidebar',
+        justifySelf: 'end'
+       }
+    })
+  }
+
+  // clickHandlerRight = () =>{
+  //   this.setState({
+  //     style2: {
+  //       ...this.state.style2,
+  //       gridArea: 'sidebar',
+  //       justifySelf: 'end'
+  //     }
+  //  })
+  // }
+
+  // onclickFunc = ()=>{
+  //   this.clickHandlerLeft();
+  //   this.clickHandlerRight();
+  // }
+
+ render() {
+    return (
+      
+      <div  onClick={this.clickHandler} className= "container">
+        <h2 className='title'>FORTUNE COOKIE</h2>
+    
+        <div style={this.state.style} className='leftCookie'>  left cookie</div>
+        <div style={this.state.style2}className='rightCookie'>right cookie</div>
+        <footer className='footer'>Footer</footer>
+      
+      </div>
+    );
+  }
 }
 
 export default App
